886a131e1a538e4b97ef7fd8249f7c60ef10ca64
Ahmed-Raza0 <ahed80174@gmail.com>
2024-09-23 19:45:03 +0500

    project

---
+++ Milestone-1/project-1/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h2 className="text-5xl text-center text-red-500">Hello World</h2>
+    </div>
+  );
+}
