43a98a65946c7fe8dcf21720062eba3c3a265e11
Ahmed-Raza0 <ahed80174@gmail.com>
2024-11-16 12:01:19 +0500

    update

--- Milestone-4/project/components/form-message.tsx
+++ Milestone-4/project/components/form-message.tsx
@@ -3,12 +3,14 @@ export type Message =
   | { error: string }
   | { message: string };
 
+  
+
 export function FormMessage({ message }: { message: Message }) {
   return (
     <div className="flex flex-col gap-2 w-full max-w-md text-sm">
       {"success" in message && (
         <div className="text-foreground border-l-2 border-foreground px-4">
-          {message.success}
+          {"Thanks for signing up!" }
         </div>
       )}
       {"error" in message && (
