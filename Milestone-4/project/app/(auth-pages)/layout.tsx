2ea57ca1d25e96f02756757afc05ed15ff858f74
Ahmed-Raza0 <ahed80174@gmail.com>
2024-11-16 08:20:38 +0500

    update

--- Milestone-4/project/app/(auth-pages)/layout.tsx
+++ Milestone-4/project/app/(auth-pages)/layout.tsx
@@ -1,9 +1,14 @@
+import Header from "@/components/Header";
+
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
+    <>
+    <Header/>
     <div className="w-full h-screen flex flex-col gap-6 items-center justify-center">{children}</div>
+    </>
   );
 }
